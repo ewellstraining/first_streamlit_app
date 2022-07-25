080aa7070b918b524413786a6cf2f2c232869568
ewellstraining <109914741+ewellstraining@users.noreply.github.com>
2022-07-25 17:32:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,6 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-fruit_add = streamlit.text_input('What fruit would you like to add?','Jackfruit')              # ask user for choice, default to kiwi 
-streamlit.write('Thanks for adding ', fruit_add)                                               # show what user put in
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")    # dummy value for testing
+#fruit_add = streamlit.text_input('What fruit would you like to add?','Jackfruit')              # ask user for choice, default to kiwi 
+#streamlit.write('Thanks for adding ', fruit_add)                                               # show what user put in
+#my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")    # dummy value for testing
