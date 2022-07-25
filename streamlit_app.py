f3394877775132fb6c5caf6b79372f6db9223851
ewellstraining <109914741+ewellstraining@users.noreply.github.com>
2022-07-25 12:45:11 -0500

    Update streamlit_app.py
    change to kiwi, prep for swapping in a variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display FruityVice API response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json())  # don't want to show this any more, commented it out
 
 # display API data in a pandas dataframe (table)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
